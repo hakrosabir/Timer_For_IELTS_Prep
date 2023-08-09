49033279efdc2b65611d0ef33b8d51fe9d45fccd
Sabir Ali <hakrosabir@gmail.com>
2023-08-09 08:17:49 +0000

    Update App.css and index.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,15 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
+    {/* Add meta tags for SEO optimization */}
+    <meta charSet="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Timer for IELTS Practice</title>
+    <meta
+      name="description"
+      content="Improve your IELTS preparation with this timer app. Stay focused during your study sessions."
+    />
+    {/* End of meta tags */}
     <App />
   </StrictMode>
 );
